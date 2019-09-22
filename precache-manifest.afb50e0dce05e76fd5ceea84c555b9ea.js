4bb747bf27386ff59d0c46e155610fe0ded4a46a
unknown <abanoubmaher2015@gmail.com>
2019-09-22 21:05:11 +0200

    Updates

---
+++ precache-manifest.afb50e0dce05e76fd5ceea84c555b9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99a66307c900e40edd8e6acd32b653c",
+    "url": "/toDo-list/index.html"
+  },
+  {
+    "revision": "99ae5eba817536d2abb9",
+    "url": "/toDo-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "81119ab61e6429fcbe28",
+    "url": "/toDo-list/static/css/main.4f962806.chunk.css"
+  },
+  {
+    "revision": "99ae5eba817536d2abb9",
+    "url": "/toDo-list/static/js/2.f00d77b8.chunk.js"
+  },
+  {
+    "revision": "81119ab61e6429fcbe28",
+    "url": "/toDo-list/static/js/main.5ed999e8.chunk.js"
+  },
+  {
+    "revision": "e53ad5f0d6f4c93b5ec8",
+    "url": "/toDo-list/static/js/runtime~main.78441855.js"
+  }
+]);
